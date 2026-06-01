dd6c9519ba69be0ecd28a57eae8c2d858ec9392a
Fs1ooz <spinaf64@gmail.com>
2026-06-01 17:58:22 +0200

    Aggiornamento app

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "f35bd288b5ad2cc562888179ee1b24ac",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "c92f5aacb51437e65b9188f25cedee75",
+"flutter_bootstrap.js": "612b67e9b33083e2823debea3121ea37",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
